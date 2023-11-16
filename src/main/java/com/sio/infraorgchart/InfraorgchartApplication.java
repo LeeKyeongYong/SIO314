a15eabe5f9fa3bafe8996710cfe6bd8b69f30eb5
이경용 <sleekydz86@icloud.com>
2023-11-16 19:59:08 +0900

    조직도 셋팅
    조직도 프로젝트 셋팅

---
+++ src/main/java/com/sio/infraorgchart/InfraorgchartApplication.java
@@ -0,0 +1,13 @@
+package com.sio.infraorgchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraorgchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraorgchartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sio/infraorgchart/InfraorgchartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sio.infraorgchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraorgchartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
